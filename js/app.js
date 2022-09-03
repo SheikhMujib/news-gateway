57134a09ed32daac13d47d6ec6090d9d00f02fc0
SheikhMujib <mbasheikhmujib@gmail.com>
2022-09-03 15:36:19 +0600

    News title, thumbnail and details added

--- js/app.js
+++ js/app.js
@@ -41,9 +41,7 @@ const displayNews = cards => {
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title">${card.title}</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to
-                                    additional content. This content is a little bit longer.</p>
+                    <p class="card-text">${card.details}</p>
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
